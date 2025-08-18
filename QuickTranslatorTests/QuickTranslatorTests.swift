c70260a28d7bfa3ab4e29dd6c65bd64ee7978380
Veysel Bozkurt <veyysellbozkrt@gmail.com>
2025-08-18 15:16:06 +0300

    Initial Commit

---
+++ QuickTranslatorTests/QuickTranslatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickTranslatorTests.swift
+//  QuickTranslatorTests
+//
+//  Created by Veysel Bozkurt on 18.08.2025.
+//
+
+import Testing
+@testable import QuickTranslator
+
+struct QuickTranslatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
